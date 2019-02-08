55505374366f6ced103445901a96ffa95ecea49b
Jeya Maria Jose <jeyamariajose7@gmail.com>
2019-02-08 22:51:28 +0530

    added Mean BP calc

--- models_ML/linreg.py
+++ models_ML/linreg.py
@@ -4,16 +4,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression  
 import numpy as np  
 from sklearn.metrics import mean_squared_error, r2_score
+import csv
 
-dataset = pd.read_csv('Total.csv',names = ['alpha','PIR', 'ptt', 'bpmax' ,'bpmin', 'hrfinal', 'ih', 'il', 'meu'])
+dataset = pd.read_csv('cleaned_further.csv',names = ['alpha','PIR', 'ptt', 'bpmax' ,'bpmin', 'hrfinal', 'ih', 'il', 'meu', 'j', 'k','l','m','n','o','p','q','r','s'])
+sbp = list()
+dbp = list()
+real_BP = list()
 
-X = dataset[['alpha','PIR', 'ptt','hrfinal', 'ih', 'il', 'meu']]
+X = dataset[['alpha','PIR', 'ptt','hrfinal', 'ih', 'il', 'meu','j', 'k','l','m','n','o','p','q','r','s']]
 
-y = dataset[['bpmax', 'bpmin']]
+with open('cleaned_further.csv', 'r') as csvfile:
+	csv_reader = csv.reader(csvfile, delimiter = ',')
+	print csv_reader
+	for row in csv_reader:
+		#ptt.append(float(row[2]))
+		sbp.append(float(row[3]))
+		dbp.append(float(row[4]))
 
+	real_BP = list()
+	for i in range(len(sbp)):
+		BP_actual = (2*dbp[i] + sbp[i])/3
+		real_BP.append(BP_actual)
 
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0) 
+X_train, X_test, y_train, y_test = train_test_split(X, real_BP, test_size=0.2, random_state=0) 
 
 from sklearn.preprocessing import StandardScaler
 sc_X=StandardScaler()
@@ -31,7 +44,7 @@ regressor.fit(x_train, y_train)
 y_pred = regressor.predict(x_test)
 
 
-print('Coefficients: \n', regressor.coef_)
+#print('Coefficients: \n', regressor.coef_)
 # The mean squared error
 from sklearn import metrics  
 
