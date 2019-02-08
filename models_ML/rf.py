40e3a5aad8758385cea7b6708abf53e7935e44e2
Jeya Maria Jose <jeyamariajose7@gmail.com>
2019-02-08 22:51:56 +0530

    added mean bp

--- models_ML/rf.py
+++ models_ML/rf.py
@@ -4,16 +4,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression  
 import numpy as np  
 from sklearn.metrics import mean_squared_error, r2_score
-
-dataset = pd.read_csv('Total.csv',names = ['alpha','PIR', 'ptt', 'bpmax' ,'bpmin', 'hrfinal', 'ih', 'il', 'meu'])
-
-X = dataset[['alpha','PIR', 'ptt']]
-
-y = dataset[['bpmax', 'bpmin']]
+import csv
+dataset = pd.read_csv('cleaned_further.csv',names = ['alpha','PIR', 'ptt', 'bpmax' ,'bpmin', 'hrfinal', 'ih', 'il', 'meu', 'j', 'k','l','m','n','o','p','q','r','s'])
 
 
+X = dataset[['alpha','PIR', 'ptt','hrfinal', 'ih', 'il', 'meu','j', 'k','l','m','n','o','p','q','r','s']]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0) 
+y = dataset[['bpmin','bpmax']]
+
+sbp = list()
+dbp = list()
+real_BP = list()
+with open('cleaned_further.csv', 'r') as csvfile:
+	csv_reader = csv.reader(csvfile, delimiter = ',')
+	print csv_reader
+	for row in csv_reader:
+		#ptt.append(float(row[2]))
+		sbp.append(float(row[3]))
+		dbp.append(float(row[4]))
+
+	real_BP = list()
+	for i in range(len(sbp)):
+		BP_actual = (2*dbp[i] + sbp[i])/3
+		real_BP.append(BP_actual)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, real_BP, test_size=0.2, random_state=0) 
 
 from sklearn.preprocessing import StandardScaler
 sc_X=StandardScaler()
@@ -23,7 +39,7 @@ x_test=sc_X.transform(X_test)
 
 
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 500, random_state = 0)
+regressor = RandomForestRegressor(n_estimators = 500, random_state = 2)
 
 #regressor = LinearRegression() 
 #print dataset.isnull().any()
